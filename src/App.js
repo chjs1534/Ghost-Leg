c00411ff536a4db225d263dbfca2ab12c4287ae8
Codey Suh <codeysuh2002@gmail.com>
2023-01-31 18:01:50 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './styles.css';
 
-function App() {
+const Setter = () => {
+  const [count, setCount] = useState(2)
+
+  const increment = () => {
+    setCount(count + 1);
+    // setTopRow(oldTopRow => [...oldTopRow, <input type="text"></input>])
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+    // setTopRow(oldTopRow => oldTopRow.slice(0, -1))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <button onClick={decrement} className="button">-</button>
+      <h1>{count}</h1>
+      <button onClick={increment} className="button">+</button>
     </div>
-  );
+  )
+}
+
+const Box = () => {
+  const [rungs, setRungs] = 
+    useState([
+      <div className="rung"></div>,
+      <div className="rung"></div>
+    ])
+  const [columns, setColumns] = 
+    useState([
+      <div className="column">
+        <input type="text"></input>
+        <div className="vertLineRight">
+          {rungs}
+        </div>
+        <input type="text"></input>
+      </div>,
+      <div className="column">
+        <input type="text"></input>
+        <div className="vertLineLeft">
+          {rungs}
+        </div>
+        <input type="text"></input>
+      </div>
+    ])
+
+
+  return (
+    <div className="box">
+      {columns}
+    </div>
+  )
+}
+
+const Result = () => {
+  return (
+    <div className="result">
+      <ul className="resultList">
+        <li>Hello World</li>
+        <li>Hello World2</li>
+      </ul>
+    </div>
+  )
+}
+
+const App = () => {
+    return (
+      <div className="page">
+    <Setter />
+    <Box />
+    <Result />
+      </div>
+    )
 }
 
 export default App;
